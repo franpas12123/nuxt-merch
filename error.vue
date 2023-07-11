ce37fe4c1e44368b880535f0dca45a95e622188b
Frances Pasulohan <franpas12123@gmail.com>
2023-07-11 22:24:05 +0800

    create a simple custom error page

---
+++ error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mt-7 max-w-md mx-auto text-center card">
+    <p class="mt-7 text-7xl font-bold">{{ error.statusCode }}</p>
+    <p class="mt-7">{{ error.message }}</p>
+    <button class="btn my-7" @click="handleClearError">Go Back</button>
+  </div>
+</template>
+
+<script setup>
+defineProps(['error'])
+const router = useRouter()
+const handleClearError = () => {
+  router.back()
+}
+</script>
+
+<style lang="scss" scoped></style>
