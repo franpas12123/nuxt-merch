42e06f2156bf861639cb0e470fe44c0210c15d19
Frances Pasulohan <franpas12123@gmail.com>
2023-07-11 22:32:08 +0800

    create about page

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="">
+    <h1 class="text-2xl py-3">ABOUT</h1>
+    <h2 class="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis iste eum doloribus a atque ducimus
+      molestias praesentium repellat sequi omnis libero cupiditate cum veniam itaque, molestiae obcaecati. Animi, quos
+      quis!</h2>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<!-- <style scoped></style> -->
