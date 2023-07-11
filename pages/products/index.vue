b1c2f0aa38a0c303dd248128a7daff431de4965e
Frances Pasulohan <franpas12123@gmail.com>
2023-07-11 21:45:35 +0800

    create index page for products

---
+++ pages/products/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="">
+    <h1 class="font-bold">PRODUCTS</h1>
+
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="product in products" :key="`p-${p.id}-${product?.title}`">
+        <ProductCard :product="product" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'products'
+})
+
+// fetch products from fakestore api
+const { data: products } = await useFetch('https://fakestoreapi.com/products')
+</script>
+
+<!-- <style scoped></style> -->
