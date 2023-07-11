dc3e1beca9a722554ae6802aefb1db1611051498
Frances Pasulohan <franpas12123@gmail.com>
2023-07-11 20:53:50 +0800

    initialize index.vue

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">
+    <h1 class="font-bold">WELCOME</h1>
+
+    <h2>this is my nuxt app!</h2>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<!-- <style scoped></style> -->
