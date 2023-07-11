7e4e362a472914520e36f62d621ab7a44dae4db2
Frances Pasulohan <franpas12123@gmail.com>
2023-07-11 22:40:05 +0800

    add a bit of content to homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="">
-    <h1 class="font-bold">WELCOME</h1>
-
-    <h2>this is my nuxt app!</h2>
+    <h1 class="text-4xl mt-20 mb-24 text-center">Welcome to my <span class="font-semibold text-[#12b488]">Nuxt Merch
+        App</span>!</h1>
+    <div class="grid grid-cols-2 gap-10 items-center">
+      <div class="flex justify-center">
+        <img src="/assets/images/kz apps.png" alt="kzapps" class="rounded-xl">
+      </div>
+      <h2 class="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis iste eum doloribus a atque ducimus
+        molestias praesentium repellat sequi omnis libero cupiditate cum veniam itaque, molestiae obcaecati. Animi, quos
+        quis!</h2>
+    </div>
   </div>
 </template>
 
