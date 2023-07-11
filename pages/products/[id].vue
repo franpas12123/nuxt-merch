9a3152a40e2ddcb4230008698f40f76436f9a66c
Frances Pasulohan <franpas12123@gmail.com>
2023-07-11 22:09:58 +0800

    throw an error if product not found

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -9,6 +9,13 @@ const { id } = useRoute().params
 
 // fetch product from fakestoreapi using id
 const { data: product } = await useFetch(`https://fakestoreapi.com/products/${id}`, { key: id })
+if (!product.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Product not found',
+    fatal: true
+  })
+}
 </script>
 
 <style lang="scss" scoped></style>
